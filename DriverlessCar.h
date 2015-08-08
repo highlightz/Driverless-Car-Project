94e072d1e409ca23a56dec60beec62e00e641423
Highlight Clark <highlightz@163.com>
2015-08-08 22:12:40 +0800

    Create DriverlessCar.h

---
+++ DriverlessCar.h
@@ -0,0 +1,16 @@
+#ifndef  DRIVERLESSCAR_H
+#define DRIVERLESSCAR_H
+
+class DriverlessCar
+{
+public:
+	// Motion Modes
+	void startRunning( );
+	void goStraight( );
+	void turnRight( );
+	void turnLeft( );
+	void stopRunning( );
+private:
+};
+
+#endif
